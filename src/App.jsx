1f4615f3bbe04f6cc4adbae4f8961ea5470133c2
shyye <emma.art.dev@gmail.com>
2024-11-12 10:00:55 +0100

    Change from dummy data to API call, add existing comments to posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Menu'
@@ -8,6 +8,9 @@ import PostsContainer from './components/posts/PostsContainer'
 const AppContext = createContext()
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
   // Dummy data for logged in user
   const loggedInUser = {
     firstname: 'John',
@@ -15,27 +18,17 @@ function App() {
     initials: 'JD',
   }
 
-  // Dummy data for posts
-  const posts = [
-    {
-      id: 1,
-      contactId: 8,
-      title: "Titulus umerus tamen asperiores adicio.",
-      content: "A verbera vociferor cicuta tepidus defetiscor vigilo curtus deputo vulgo. Spes amoveo aetas. Non curriculum aegrus utique summopere tollo. Victoria aestas clamo aeger. Aperte repellendus compello videlicet vicissitudo tertius conscendo volubilis spoliatio."
-    },
-    {
-      id: 2,
-      contactId: 5,
-      title: "Consequatur curis decretum.",
-      content: "Tenus triumphus modi cunabula tricesimus adfectus coepi demulceo dedecor turpis. Callide solio verto viridis inventore error tandem vir. Certe cenaculum spargo arbitro allatus tergo tres creo. Ait aranea socius aeneus vox trans territo corrumpo timor ultra. Certe provident usitas sint cruentus solitudo adicio utor vetus cariosus."
-    },
-    {
-      id: 3,
-      contactId: 9,
-      title: "Virgo occaecati basium textus solum adamo iusto.",
-      content: "Curtus demo cohaero impedit conatus ceno fugiat concedo. Esse corpus cohibeo caelum atrocitas. Validus beneficium ipsam textor totam iusto vado talus solium. Totus capio accendo deserunt concido cattus alo cresco. Verecundia vespillo validus thema copia copiose curtus neque unus. Sulum testimonium arto quod eos eligendi. Turba acidus thalassinus corpus volup patria."
-    }
-  ]
+  // Get Posts
+  const getData = async () => {
+    const res = await fetch("https://boolean-uk-api-server.fly.dev/shyye/post");
+    const data = await res.json();
+    setPosts(data);
+  };
+
+  // Load data
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
--- src/components/posts/PostItem.jsx
+++ src/components/posts/PostItem.jsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from "react";
 import ProfileCircle from "../profile-circle/ProfileCircle";
 
 function PostItem({ post }) {
+  const [comments, setComments] = useState([]);
+
+  // Get comments for this post
+  const getComments = async () => {
+    const res = await fetch(
+      `https://boolean-uk-api-server.fly.dev/shyye/post/${post.id}/comment`
+    );
+    const data = await res.json();
+    setComments(data);
+  };
+
+  // Load data
+  useEffect(() => {
+    getComments();
+    console.log(comments);
+  }, []);
+
   return (
     <div className="post-card-container">
+      <div className="post-header-wrapper">
+        <ProfileCircle userInitals={"Placeholder"} />
         <div>
-            <ProfileCircle userInitals={"Placeholder"}/>
-            <div>
-                <h4>{"Name"}</h4>
-                <p>{"Title"}</p>
-            </div>
+          <h4>{post.contactId}</h4>
+          <p>{post.title}</p>
         </div>
-        <div>
-            <p>{"Content"}</p>
+      </div>
+      <div>
+        <p>{post.content}</p>
+      </div>
+      <div>{comments.map((comment) => (
+        <div key={comment.id} className="post-comment-wrapper">
+          <ProfileCircle userInitals={comment.contactId} />
+          <div className="post-comment">{comment.content}</div>
         </div>
+      ))}</div>
     </div>
   );
 }
